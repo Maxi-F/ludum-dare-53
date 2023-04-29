1e55e106a5e6da50b1c33de22df2dbf2730b0a10
Maxi-F <maximiliano.a.feldman@gmail.com>
2023-04-29 20:50:19 -0300

    adding ghosts logic

---
+++ Assets/Scripts/Player/GhostInteractor.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PickUpObject))]
+[RequireComponent(typeof(LookObject))]
+[RequireComponent(typeof(ObjectDescription))]
+public class GhostInteractor : MonoBehaviour
+{
+    private PickUpObject _pickUpObject;
+    private LookObject _lookObject;
+    private ObjectDescription _objectDescription;
+
+    private void Awake()
+    {
+        _pickUpObject = GetComponent<PickUpObject>();
+        _lookObject = GetComponent<LookObject>();
+        _objectDescription = GetComponent<ObjectDescription>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (_lookObject.isLookingToObject)
+        {
+            Ghost ghost = _lookObject.lookedObject.transform.GetComponent<Ghost>();
+            if (ghost)
+            {
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    if (_pickUpObject.heldObject == null)
+                    {
+                        _objectDescription.ShowDescription(ghost.wantedObjectDescription);
+                    }
+                    else
+                    {
+                        ghost.ObtainObject(_pickUpObject.heldObject);
+                        _pickUpObject.DropObject();
+                    }
+                }
+            }
+        }
+    }
+}
